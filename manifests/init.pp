65b80a597c74fd951669550338c12818283c36f4
icot <icot@users.noreply.github.com>
2014-10-27 23:07:49 +0100

    Manifest setting up single config files

---
+++ manifests/init.pp
@@ -0,0 +1,39 @@
+
+class dotfiles ($home = '/tmp') {
+
+  $files = {
+    'msmtprc'    => {
+        mode    => 0600,
+        path    => "${home}/.msmtprc",
+        content => template('dotfiles/msmtprc')},
+    'Xresources' => {
+        mode    => 0644,
+        path    => "${home}/.Xresources",
+        content => template('dotfiles/Xresources')},
+    'vimrc'      => {
+        mode    => 0644,
+        path    => "${home}/.vimrc",
+        content => template('dotfiles/vimrc')},
+    'zshrc'      => {
+        mode    => 0644,
+        path    => "${home}/.zshrc",
+        content => template('dotfiles/zshrc')},
+    'my.cnf'     => {
+        mode    => 0600,
+        path    => "${home}/.my.cnf",
+        content  => template('dotfiles/my.cnf')},
+    'psqlrc'    => {
+        mode    => 0644,
+        path    => "${home}/.psqlrc",
+        content => template('dotfiles/psqlrc')},
+    'gitconfig'  => {
+        mode    => 0644,
+        path    => "${home}/.gitconfig",
+        content => template('dotfiles/gitconfig')},
+  }
+
+  create_resources(file, $files)
+
+}
+
+
