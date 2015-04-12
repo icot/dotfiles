18559a5a316c0ff7b064673a5b55af820d9d03fd
icot <icot@users.noreply.github.com>
2015-04-12 19:09:40 +0200

    XMONAD: Tint + Conky configuration

--- files/xmonad/xmonad.hs
+++ files/xmonad/xmonad.hs
@@ -5,6 +5,7 @@ import XMonad.Layout.NoBorders
 import XMonad.Layout.ShowWName
 import XMonad.Layout.Gaps
 import XMonad.Layout.Grid
+import XMonad.Layout.ThreeColumns
 import XMonad.Actions.CycleWS
 import qualified XMonad.StackSet as W
 import qualified Data.Map as M
@@ -17,10 +18,17 @@ import System.IO
 
 -- Java GUI Problems
 import XMonad.Hooks.SetWMName
---
+-- Tint2
+import XMonad.Hooks.EwmhDesktops
+
+-- Colors
+fgColor = "#eee8d5"
+fgInactive = "#b58900"
+fgActive = "#dc322f"
+fgEmpty = "#93a1a1"
+bgColor = "#002b36"
 
 -- My Manage Hook
-
 myManageHook = composeAll
         [ className =? "Iceweasel" --> doShift "1:Web"
         , className =? "Icedove" --> doShift "9:Mail"
@@ -28,50 +36,27 @@ myManageHook = composeAll
         , className =? "Xmessage" --> doFloat
         , manageDocks
         ]
-        
-myLogHook :: Handle -> X ()
-myLogHook h = dynamicLogWithPP $ defaultPP
-    {
-    ppCurrent           =   dzenColor "white" "#1B1D1E" . pad
-    , ppVisible           =   dzenColor "white" "#1B1D1E" . pad
-    , ppHidden            =   dzenColor "grey" "#1B1D1E" . pad
-    , ppHiddenNoWindows   =   dzenColor "#7b7b7b" "#1B1D1E" . pad
-    , ppUrgent            =   dzenColor "#ff0000" "#1B1D1E" . pad
-    , ppWsSep             =   ""
-    , ppSep               =   "  |  "
-    , ppLayout            =   dzenColor "green" "#1B1D1E" .
-                              (\x -> case x of
-                                  "ResizableTall"             ->      "RT"
-                                  "Mirror ResizableTall"      ->      "MRT"
-                                  "Full"                      ->      "F"
-                                  "Simple Float"              ->      "SF"
-                                  _                           ->      x
-                              )
---    , ppTitle             =   (" " ++) . dzenColor "green" "#1B1D1E" . dzenEscape . shorten 80
-    , ppOrder = \xs -> take 2 xs ++ drop 3 xs
-    , ppExtras = 
-            [ 
-            -- dzenColorL "grey" "#1B1D1E" fixedWidthL AlignCenter " " 60 logTitle
-            fixedWidthL AlignCenter " " 60 logTitle
-            , loadAvg
-            , date "%Y-%m-%d %R:%S" 
-            ] 
-    , ppOutput            =   hPutStrLn h
-    }
 
-myXmonadBar = "dzen2 -x '0' -y '1180' -h '20' -w '1920' -ta 'l' -fg 'grey' -bg '#1B1D1E' -fn inconsolata:size=10"
-myStatusBar = "conky -c ~/.xmonad/.conky_dzen | dzen2 -x '800' -y '1056' -w '640' -h '20' -ta 'r' -bg '#1B1D1E' -fg '#FFFFFF' -y '0'"
-myTrayer = "killall trayer; trayer --edge top --align right --widthtype request --SetDockType true --SetPartialStrut true --transparent true --alpha 0 --tint 0x000000 --expand true --heighttype pixel --height 16"
+myApps = 
+    [ "~/bin/monitor"
+    , "urxvtd"
+    , "tint2"
+    , "xscreensaver"
+    , "xfce4-power-manager"
+    , "nm-applet"
+    , "feh --bg-center ~/Documents/wallpapers/kanagawa.jpg"
+    , "./dropbox-dist/dropboxd"
+    , "conky"
+    ]
 
 main = do
-    dzenLeftBar <- spawnPipe myXmonadBar
---    dzenRightBar <- spawnPipe myStatusBar
-    xmonad $ defaultConfig
-            { workspaces = ["1:Web","2:Consoles","3:","4:","5:","6:","7:","8:Misc","9:Mail"]
+    mapM spawnPipe myApps
+    xmonad $ ewmh defaultConfig
+            { workspaces = ["1:Web","2:Cons","3:VLC","4:","5:","6:","7:","8:Torrent","9:Mail"]
             , terminal = "urxvtc -tr"
             , manageHook = myManageHook <+> manageDocks <+> manageHook defaultConfig
             , layoutHook = showWName' mySWNConfig myLayout
-            , logHook = myLogHook dzenLeftBar >> fadeInactiveLogHook 0xdddddddd
+            , logHook = ewmhDesktopsLogHook
             -- keys
             , modMask = mod4Mask
             , keys = \c -> myKeys c `M.union` keys defaultConfig c
@@ -88,11 +73,13 @@ mySWNConfig = defaultSWNConfig
         , swn_color = "white"
         , swn_fade = 1/4 }
 
-myLayout = avoidStruts $ smartBorders $ (normalTiled ||| Mirror normalTiled ||| myTabbed ||| myGrid)
+myLayout = avoidStruts $ smartBorders $ (normalTiled ||| Mirror normalTiled 
+        ||| myTabbed ||| myGrid ||| my3col )
     where 
         normalTiled = Tall 1 (2/100) (1/2)
         myTabbed = tabbed shrinkText myTabConfig
         myGrid = Grid
+        my3col = ThreeCol 1 (3/100) (1/2)
 
 myTabConfig = defaultTheme
         { activeColor = "#3cb371"
@@ -109,7 +96,8 @@ myKeys conf@(XConfig {modMask = modm}) =
         , ((modm, xK_s), spawn "urxvtc -e byobu")
         , ((modm, xK_f), spawn "iceweasel")
         , ((modm, xK_m), spawn "icedove")
-        , ((modm, xK_v), spawn "gvim")
+        , ((modm, xK_v), spawn "vlc")
+        , ((modm, xK_g), spawn "gvim")
         , ((modm, xK_b), do
             sendMessage ToggleStruts
             sendMessage ToggleGaps)
