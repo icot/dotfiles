100ad6946910b979ede666c5531b112b5f050765
icot <icot@users.noreply.github.com>
2018-06-04 09:22:59 +0200

    Update packages

--- manifests/packages.pp
+++ manifests/packages.pp
@@ -4,6 +4,23 @@ class dotfiles::packages {
   # Install packages
   $packages = hiera('packages')
   create_resources(package, $packages)
+  
+  $root_files = {
+    'xmonad-session'   => {
+        owner              => root,
+        group              => root,
+        mode               => 0644,
+        path               => "/usr/bin/xmonad-session",
+        source             => 'puppet:///modules/dotfiles/xmonad/xmonad-session'},
+    'xmonad.desktop'   => {
+        owner              => root,
+        group              => root,
+        mode               => 0644,
+        path               => "/usr/share/xsessions/xmonad.desktop",
+        source             => 'puppet:///modules/dotfiles/xmonad/xmonad-desktop'},
+  }
+  create_resources(file, $root_files)
+
 
 }
 
