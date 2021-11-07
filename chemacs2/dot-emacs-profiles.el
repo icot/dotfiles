863b2d88dc3a7ab3e0bf550e671b866d55a4822b
icot <icot@users.noreply.github.com>
2021-11-07 20:30:02 +0100

    Add chemacs2 config

---
+++ chemacs2/dot-emacs-profiles.el
@@ -0,0 +1,4 @@
+(("doom" . ((user-emacs-directory . "~/.emacs.doom")
+           (env . (("DOOMDIR" . "~/.doom.d")))))
+ ("prelude" . ((user-emacs-directory . "~/.emacs.prelude")))
+ ("vanilla" . ((user-emacs-directory . "~/.emacs.custom"))))
