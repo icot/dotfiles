d1c16b9d13ec06208a9922f842c96a1499a4c1ff
icot <icot@users.noreply.github.com>
2021-11-07 20:49:30 +0100

    Add vanila emacs config

---
+++ emacs-custom/.emacs.custom/init.el
@@ -0,0 +1,33 @@
+;;; init.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021 Ignacio Coterillo
+;;
+;; Author: Ignacio Coterillo <https://github.com/spike>
+;; Maintainer: Ignacio Coterillo <ignacio.coterillo@gmail.com>
+;; Created: November 07, 2021
+;; Modified: November 07, 2021
+;; Version: 0.0.1
+;; Keywords: abbrev bib c calendar comm convenience data docs emulations extensions faces files frames games hardware help hypermedia i18n internal languages lisp local maint mail matching mouse multimedia news outlines processes terminals tex tools unix vc wp
+;; Homepage: https://github.com/spike/init
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+
+
+(provide 'init)
+
+;; Disable menu bar and scroll bar
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(toggle-scroll-bar -1)
+
+
+
+;;; init.el ends here
