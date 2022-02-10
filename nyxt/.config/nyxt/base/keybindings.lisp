a4029f7c637d13f1efcdbd22da91b738bc734a01
icot <icot@users.noreply.github.com>
2022-02-10 11:38:10 +0100

    nyxt: keybindings + customization

--- nyxt/.config/nyxt/base/keybindings.lisp
+++ nyxt/.config/nyxt/base/keybindings.lisp
@@ -1,6 +1,6 @@
 (in-package #:nyxt-user)
 
-;;keybinding overrides
+;;keybinding overrides 
 (define-configuration buffer
   ((override-map (let ((map (make-keymap "my-override-map")))
                    (define-key map
@@ -22,3 +22,34 @@
                      "C-s" 'query-selection-in-search-engine
                      "C-c" 'nyxt/web-mode::copy) ;;currently an override because C-c not working without it
                    map))))
+
+;; https://discourse.atlas.engineer/t/right-way-to-modify-keybindings-in-visual-mode/228
+;; visual-mode is configurable, but you need to use proper package prefix for that.
+(define-configuration nyxt/visual-mode:visual-mode
+  ((keymap-scheme (let ((scheme %slot-default%))
+                    ;; Use scheme:cua or scheme:vi-normal, if that strikes your fancy.
+                    (keymap:define-key (gethash scheme:emacs scheme)
+                      ;; Put as much keybindings as you want here.
+                      "M-C-s-z" 'reload-current-buffer)
+                    scheme))))
+
+(define-configuration nyxt/vi-mode:vi-normal-mode
+  ((keymap-scheme (let ((scheme %slot-default%))
+                    ;; Use scheme:cua or scheme:vi-normal, if that strikes your fancy.
+                    (keymap:define-key (gethash scheme:vi-normal scheme)
+                      ;; Put as much keybindings as you want here.
+                      "d" 'delete-buffer
+                      "b b" 'switch-buffer
+                      "B" 'list-bookmarks
+                      "H" 'nyxt/web-mode:list-history
+                      "M-h" 'nyxt/web-mode:scroll-left
+                      "M-l" 'nyxt/web-mode:scroll-right
+                      "h f" 'describe-function
+                      "h v" 'describe-variable
+                      "h b" 'describe-bindings
+                      "h c" 'describe-command
+                      "h a" 'describe-any
+                      "h m" 'describe-macro
+                      "M-x" 'execute-command
+                      "C-s" 'query-selection-in-search-engine)
+                    scheme))))
--- nyxt/.config/nyxt/init.lisp
+++ nyxt/.config/nyxt/init.lisp
@@ -15,7 +15,9 @@
 ;; Configuring Default modes
 (define-configuration buffer
   ((default-modes (append '(reduce-tracking-mode ;; Tracking
-                            blocker-mode) ;; Ad blocking
+                            blocker-mode ;; Ad blocking
+                                        ;    nyxt::vi-normal-mode) ;; VI Mode
+                            )
                           %slot-default%))))
 
 ;;setting new buffer url and having nyxt start full screen
@@ -25,6 +27,10 @@
 ;  (let ((window (current-window)))
 ;    (window-set-buffer window (make-buffer :url (quri:uri "https://nyxt.atlas.engineer/")))
 ;    (toggle-fullscreen window)))
+
+
+(echo "Start swank server")
+(start-swank)
 
 ;;when reloading init.lisp file shows in message bar once finished
 (echo "Loaded config.")
