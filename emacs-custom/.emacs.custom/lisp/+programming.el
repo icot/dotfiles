378b9b49cbd5ed9d4e2400c4981a609a79477343
icot <icot@users.noreply.github.com>
2022-08-03 15:26:27 +0200

    emacs: Switch from lispy to parinfer-rust

--- emacs-custom/.emacs.custom/lisp/+programming.el
+++ emacs-custom/.emacs.custom/lisp/+programming.el
@@ -64,34 +64,10 @@
 
 ;;; LISPs
 
-(use-package lispy
-  :defer t)
-
-;; Parinfer (archived)
-;; https://github.com/DogLooksGood/parinfer-mode 
-
-;(use-package parinfer
-;  :defer t
-;  :bind
-;  (("C-," . parinfer-toggle-mode))
-;  :init
-;  (progn
-;    (setq parinfer-extensions
-;          '(defaults       ; should be included.
-;            pretty-parens  ; different paren styles for different modes.
-;            evil           ; If you use Evil.
-;            lispy          ; If you use Lispy. With this extension, you should install Lispy and do not enable lispy-mode directly.
-;            paredit        ; Introduce some paredit commands.
-;            smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
-;            smart-yank   ; Yank behavior depend on mode.
-;    (add-hook 'clojure-mode-hook #'parinfer-mode)
-;    (add-hook 'emacs-lisp-mode-hook #'parinfer-mode)
-;    (add-hook 'common-lisp-mode-hook #'parinfer-mode)
-;    (add-hook 'scheme-mode-hook #'parinfer-mode)
-;    (add-hook 'lisp-mode-hook #'parinfer-mode))
-
-
-;; https://github.com/justinbarclay/parinfer-rust-mode
+(use-package parinfer-rust-mode
+  :hook (emacs-lisp-mode clojure-mode common-lisp scheme-mode lisp-mode)
+  :config
+  (setq parinfer-rust-auto-download t))
 
 ;; LSP Mode
 
@@ -120,15 +96,14 @@
 
 ;; Servers
 
-
 ;; Python
 ;;; Pyright https://emacs-lsp.github.io/lsp-pyright/
 
 (use-package lsp-pyright
   :ensure t
   :hook (python-mode . (lambda ()
-                          (require 'lsp-pyright)
-                          (lsp-deferred))))
+                         (require 'lsp-pyright)
+                         (lsp-deferred))))
 
 ;; Java
 (use-package lsp-java
