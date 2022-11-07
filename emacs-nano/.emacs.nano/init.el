87079252aafcb049688297c169d91c647e08ec6d
icot <icot@users.noreply.github.com>
2022-11-07 13:09:45 +0100

    Add lambda-line. Change face size

--- emacs-nano/.emacs.nano/init.el
+++ emacs-nano/.emacs.nano/init.el
@@ -119,7 +119,7 @@
 (setq modus-themes-bold-constructs t)
 (setq modus-themes-italic-constructs t)
 
-(set-face-attribute 'default nil :height 105)
+(set-face-attribute 'default nil :height 120)
 
 ;; Install lambda-themes
 (use-package lambda-themes
@@ -140,6 +140,31 @@
 (use-package catppucin-latte
   :straight (:host nil :repo "https://github.com/catppuccin/emacs"))
 
+;; Install and configure lambda-line
+(use-package lambda-line
+  :straight (:type git :host github :repo "lambda-emacs/lambda-line") 
+  :custom
+  (lambda-line-icon-time t) ;; requires ClockFace font (see below)
+  (lambda-line-clockface-update-fontset "ClockFaceRect") ;; set clock icon
+  (lambda-line-position 'bottom) ;; Set position of status-line 
+  (lambda-line-abbrev t) ;; abbreviate major modes
+  (lambda-line-hspace "  ")  ;; add some cushion
+  (lambda-line-prefix t) ;; use a prefix symbol
+  (lambda-line-prefix-padding nil) ;; no extra space for prefix 
+  (lambda-line-status-invert nil)  ;; no invert colors
+  (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
+  (lambda-line-gui-mod-symbol " ⬤") 
+  (lambda-line-gui-rw-symbol  " ◯") 
+  (lambda-line-space-top +.20)  ;; padding on top and bottom of line
+  (lambda-line-space-bottom -.20)
+  (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
+  :config
+  ;; activate lambda-line 
+  (lambda-line-mode) 
+  ;; set divider line in footer
+  (when (eq lambda-line-position 'top)
+    (setq-default mode-line-format (list "%_"))
+    (setq mode-line-format (list "%_"))))
 
 ;; Window/frame setup management
 
