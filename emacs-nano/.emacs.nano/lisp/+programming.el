0666b1a04b6d73354ee814f78fb07d0d54c1c99b
icot <icot@users.noreply.github.com>
2022-11-14 06:53:56 +0100

    emacs: temptative gopls configuration

--- emacs-nano/.emacs.nano/lisp/+programming.el
+++ emacs-nano/.emacs.nano/lisp/+programming.el
@@ -221,14 +221,30 @@
 (use-package puppet-mode
   :defer t)
 
-;; golang
+;;; golang
+
 (use-package go-mode
   :defer t
   :ensure t)
 
+;; https://github.com/golang/tools/blob/master/gopls/doc/emacs.md
+;; go install golang.org/x/tools/gopls@latest
+
+(add-hook 'go-mode-hook #'lsp-deferred)
+
+;; Set up before-save hooks to format buffer and add/delete imports.
+;; Make sure you don't have other gofmt/goimports hooks enabled.
+
+(defun lsp-go-install-save-hooks ()
+  (add-hook 'before-save-hook #'lsp-format-buffer t t)
+  (add-hook 'before-save-hook #'lsp-organize-imports t t))
+(add-hook 'go-mode-hook #'lsp-go-install-save-hooks)
+
+
+
+
 ;; c/c++
 
-;; go
 ;;(use-package company-shell
 ;;  :defer t)
 
