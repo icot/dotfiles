104b379cd413f4611c0de2b5e2fa487421cbad9e
icot <icot@users.noreply.github.com>
2022-12-11 18:24:51 +0100

    emacs/git: Add forge and lab.el configuration

--- emacs-nano/.emacs.nano/lisp/+git.el
+++ emacs-nano/.emacs.nano/lisp/+git.el
@@ -7,17 +7,35 @@
   :custom
   (magit-clone-default-directory "~/workspace/puppet/"))
 
+(use-package forge
+  :after magit
+  :config
+  (add-to-list 'forge-alist
+               '("gitlab.cern.ch" "gitlab.cern.ch/api/v4" "gitlab.cern.ch" 'forge-gitlab-repository)))
+  
+(use-package git-link
+  :custom
+  (git-link-use-commit t))
+
+(use-package lab
+  :ensure t
+  :straight (:host github :repo "isamert/lab.el")
+  :config  (setq lab-host "https://gitlab.cern.ch"
+                 lab-token (password-store-get "gitlab/magit-forge")
+                 lab-group "db"
+                 lab-projects-directory "~/workspace"))
+
 (defun icot/clone-module (module-name)
-  "Clone a Puppet module from gitlab.cern.ch/ai"
-  (interactive "sModule name: ")
-  (let ((magit-clone-url-format "ssh://%h/%n.git")
-        (magit-clone-set-remote.pushDefault t)
-        (repo-name (concat "it-puppet-module-" module-name)))
-    (magit-clone-internal
-     ;; Using an internal here, see  https://github.com/magit/magit/discussions/4335
-     (magit-clone--name-to-url repo-name)
-     (concat magit-clone-default-directory repo-name)
-     nil)))
+ "Clone a Puppet module from gitlab.cern.ch/ai"
+ (interactive "sModule name: ")
+ (let ((magit-clone-url-format "ssh://%h/%n.git")
+       (magit-clone-set-remote.pushDefault t)
+       (repo-name (concat "it-puppet-module-" module-name)))
+   (magit-clone-internal
+    ;; Using an internal here, see  https://github.com/magit/magit/discussions/4335
+    (magit-clone--name-to-url repo-name)
+    (concat magit-clone-default-directory repo-name)
+    nil)))
 
 (defun icot/clone-hostgroup (hostgroup-name)
   "Clone a Puppet top-level hostgroup from gitlab.cern.ch/ai"
