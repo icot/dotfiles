2c7f6c35c4338281768c623228013369b4737b94
icot <icot@users.noreply.github.com>
2023-04-16 15:10:23 +0200

    emacs: switch from lambda-line to moody/minions

--- emacs-nano/.emacs.nano/init.el
+++ emacs-nano/.emacs.nano/init.el
@@ -162,31 +162,18 @@
 (use-package all-the-icons
   :ensure t)
 
-;; Install and configure lambda-line
-(use-package lambda-line
-  :straight (:type git :host github :repo "lambda-emacs/lambda-line") 
-  :custom
-  (lambda-line-icon-time t) ;; requires ClockFace font (see below)
-  (lambda-line-clockface-update-fontset "ClockFaceRect") ;; set clock icon
-  (lambda-line-position 'bottom) ;; Set position of status-line 
-  (lambda-line-abbrev t) ;; abbreviate major modes
-  (lambda-line-hspace "  ")  ;; add some cushion
-  (lambda-line-prefix t) ;; use a prefix symbol
-  (lambda-line-prefix-padding nil) ;; no extra space for prefix 
-  (lambda-line-status-invert nil)  ;; no invert colors
-  (lambda-line-gui-ro-symbol  " ⨂") ;; symbols
-  (lambda-line-gui-mod-symbol " ⬤") 
-  (lambda-line-gui-rw-symbol  " ◯") 
-  (lambda-line-space-top +.20)  ;; padding on top and bottom of line
-  (lambda-line-space-bottom -.20)
-  (lambda-line-symbol-position 0.1) ;; adjust the vertical placement of symbol
+;; Modeline
+
+(use-package moody
   :config
-  ;; activate lambda-line 
-  (lambda-line-mode) 
-  ;; set divider line in footer
-  (when (eq lambda-line-position 'top)
-    (setq-default mode-line-format (list "%_"))
-    (setq mode-line-format (list "%_"))))
+  (setq x-underline-at-descent-line t)
+  (moody-replace-mode-line-buffer-identification)
+  (moody-replace-vc-mode)
+  (moody-replace-eldoc-minibuffer-message-function))
+
+(use-package minions
+  :ensure t)
+(minions-mode)
 
 ;; Window/frame setup management
 
