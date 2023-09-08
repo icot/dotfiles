0e48ee462bbb1136c60828026fd5f071c02d6ce6
icot <icot@users.noreply.github.com>
2023-09-08 08:29:05 +0200

    emacs: Update straight bootstrap recipe

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -59,6 +59,7 @@
 (unless package-archive-contents
   (package-refresh-contents))
 
+;; Straight Bootstrap
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -66,7 +67,7 @@
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
