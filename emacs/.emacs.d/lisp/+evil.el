a2041b5f06b8003ab7e55d2f92d34bd5b35237ee
icot <icot@users.noreply.github.com>
2024-01-08 17:41:28 +0100

    WIP: evil mode additions

--- emacs/.emacs.d/lisp/+evil.el
+++ emacs/.emacs.d/lisp/+evil.el
@@ -1,3 +1,4 @@
+;; https://systemcrafters.net/emacs-from-scratch/key-bindings-and-evil/
 
 (use-package evil
   :ensure t
@@ -11,5 +12,13 @@
 (evil-define-key 'normal 'global (kbd "<leader>:") 'eval-expression)
 
 
+;; "wH" '(evil-window-move-far-left :which-key "move window to the left")
+;; "wJ" '(evil-window-move-very-bottom :which-key "move window to bottom")
+;; "wK" '(evil-window-move-very-top :which-key "move window to the top")
+;; "wL" '(evil-window-move-far-right :which-key "move window to the right")
+;; "wn" '(evil-window-new :which-key "new window")
 
+(global-set-key (kbd "C-x |") #'split-window-horizontally)
+(global-set-key (kbd "C-x _") #'split-window-vertically)
+(global-set-key (kbd "C-x =") #'balance-windows)
   
