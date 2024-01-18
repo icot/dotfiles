c9f251bda4e17a1f4684975509a64704fa3e7f9a
icot <icot@users.noreply.github.com>
2024-01-18 15:27:41 +0100

    emacs/blog refactor

--- emacs/.emacs.d/lisp/+blog.el
+++ emacs/.emacs.d/lisp/+blog.el
@@ -1,24 +1,6 @@
 ;;; +blog.el --- Description -*- lexical-binding: t; -*-
 
-(use-package org-static-blog
-  :defer t
-  :straight
-  (org-static-blog :type git
-		   :host github
-		   :repo "bastibe/org-static-blog"))
-
-;; TODO Move following lines to :config
-
-(setq org-static-blog-publish-title "icot.github.io"
-      org-static-blog-publish-url "https://icot.github.io/"
-      org-static-blog-publish-directory "~/workspace/org-blog/"
-      org-static-blog-posts-directory "~/workspace/org-blog/posts/"
-      org-static-blog-drafts-directory "~/workspace/org-blog/drafts/"
-      org-static-blog-enable-tags t
-      org-export-with-toc t
-      org-static-blog-use-preview t
-      org-static-blog-preview-convert-titles t
-      org-static-blog-preview-ellipsis "...")
+(setq my-blog-folder "~/workspace/icot.github.io")
 
 ;; Tags from https://jao.io/blog/2020-02-11-simplicity.html
 (setq icot-org-blog-tags
@@ -26,21 +8,38 @@
                 (string-match "tag-\\(.+\\)\\.html" f)
                 (format "<a href=\"/%s\">%s</a>"
                         f (match-string 1 f)))
-              (directory-files "~/workspace/org-blog" nil "tag-.*")))
+              (directory-files my-blog-folder nil "tag-.*")))
 
 ;; helper functions
-
 (defun blog/read-static-html (path)
   (with-temp-buffer
     (insert-file-contents path)
     (buffer-string)))
 
-;; Header
-(setq org-static-blog-page-header
-      (blog/read-static-html "~/workspace/org-blog/static/header.html"))
-
-;; Preamble
-(setq org-static-blog-page-preamble
+(use-package org-static-blog
+  :defer t
+  :straight
+  (org-static-blog :type git
+		   :host github
+		   :repo "bastibe/org-static-blog")
+  :config
+  ;; Misc
+  (setq org-static-blog-publish-title "icot.github.io"
+      org-static-blog-publish-url "https://icot.github.io/"
+      org-static-blog-publish-directory my-blog-folder
+      org-static-blog-posts-directory (format "%s/posts/" my-blog-folder) 
+      org-static-blog-drafts-directory (format "%s/drafts/" my-blog-folder)
+      org-static-blog-enable-tags t
+      org-export-with-toc t
+      org-static-blog-use-preview t
+      org-static-blog-preview-convert-titles t
+      org-static-blog-preview-ellipsis "..."
+      org-static-blog-index-front-matter nil)
+  ;; Header
+  (setq org-static-blog-page-header
+      (blog/read-static-html (format "%s/static/header.html" my-blog-folder)))
+  ;; Preamble
+  (setq org-static-blog-page-preamble
        (concat
          "<div class=\"header\">"
          "  <a href=\"https://icot.github.io\">icot.github.io</a>"
@@ -56,11 +55,10 @@
          "    | <a href=\"rss.xml\">rss</a>"
          "  </div>"
          "</div>"))
+  ;; Postamble
+  (setq org-static-blog-page-postamble
+      (blog/read-static-html (format "%s/static/postamble.html" my-blog-folder))))
 
-;; Postamble
 
-(setq org-static-blog-page-postamble
-      (blog/read-static-html "~/workspace/org-blog/static/postamble.html"))
 
-;; Front Matter (inserted into the index page between the preamble and blog posts)
-(setq org-static-blog-index-front-matter nil)
+
