724ae5cfaa5ce30f86736f18b88802abf1cdbfd6
icot <icot@users.noreply.github.com>
2024-05-01 07:29:02 +0200

    emacs/custom: Add custom-set-faces for modern-tab-bar

--- emacs/.emacs.d/custom.el
+++ emacs/.emacs.d/custom.el
@@ -11,3 +11,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+ '(modern-tab-bar ((t (:inherit (variable-pitch default) :background "#E0E0E0" :foreground "#000000" :box (:line-width (2 . 2) :style flat-button) :weight light)))))
