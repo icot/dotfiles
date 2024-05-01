01816f928d2a9d274683e66ed9476dc23764cfe8
icot <icot@users.noreply.github.com>
2024-05-01 15:06:25 +0200

    emacs/themes: Add tabspaces

--- emacs/.emacs.d/lisp/+themes.el
+++ emacs/.emacs.d/lisp/+themes.el
@@ -132,3 +132,23 @@
 ;;  ;; Your init file should contain only one such instance.
 ;;  ;; If there is more than one, they won't work right.
 ;;  '(modern-tab-bar ((t (:inherit (variable-pitch default) :background "#E0E0E0" :foreground "#000000" :box (:line-width (2 . 2) :style flat-button) :weight light)))))
+
+;; Tab Spaces
+(use-package tabspaces
+  ;; use this next line only if you also use straight, otherwise ignore it. 
+  :straight (:type git :host github :repo "mclear-tools/tabspaces")
+  :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup. 
+  :commands (tabspaces-switch-or-create-workspace
+             tabspaces-open-or-create-project-and-workspace)
+  :custom
+  (tabspaces-use-filtered-buffers-as-default t)
+  (tabspaces-default-tab "Default")
+  (tabspaces-remove-to-default t)
+  (tabspaces-include-buffers '("*scratch*"))
+  (tabspaces-initialize-project-with-todo t)
+  (tabspaces-todo-file-name "project-todo.org")
+  ;; sessions
+  (tabspaces-session t)
+  (tabspaces-session-auto-restore t)
+  :config
+  (tabspaces-mode))
